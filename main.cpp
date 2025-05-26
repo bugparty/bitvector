4c5ef431b3d2e3587a6845438641fd184957a21b
bowman <fancycode@gmail.com>
2025-05-26 12:32:55 -0700

    Refactor benchmark functions and remove unused test file

--- main.cpp
+++ main.cpp
@@ -4,22 +4,24 @@
 #include <chrono>
 #include <iostream>
 #include <vector>
-void benchOrig(){
-    constexpr size_t SIZE = 10000000;  // 1000万元素大小
 
-    // 定义一个vector<bool>
+// Benchmarks the performance of std::vector<bool> for setting, accessing, and traversing elements.
+void benchmarkStdVectorBool(){
+    constexpr size_t SIZE = 10000000;  // 10 million elements
+
+    // Define a vector<bool>
     std::vector<bool> bool_vector(SIZE);
 
-    // 计时设置每个元素为true的时间
+    // Time setting each element to true
     auto start_set = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < SIZE; ++i) {
         bool_vector[i] = i&0x1;
     }
     auto end_set = std::chrono::high_resolution_clock::now();
     auto duration_set = std::chrono::duration_cast<std::chrono::milliseconds>(end_set - start_set);
-    std::cout << "Setting all elements took " << duration_set.count() << " milliseconds.\n";
+    std::cout << "Setting all elements took " << duration_set.count() << " milliseconds.\\n";
 
-    // 计时访问每个元素的时间
+    // Time accessing each element
     size_t true_count = 0;
     auto start_access = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < SIZE; ++i) {
@@ -29,9 +31,9 @@ void benchOrig(){
     }
     auto end_access = std::chrono::high_resolution_clock::now();
     auto duration_access = std::chrono::duration_cast<std::chrono::milliseconds>(end_access - start_access);
-    std::cout << "Accessing all elements took " << duration_access.count() << " milliseconds.\n";
+    std::cout << "Accessing all elements took " << duration_access.count() << " milliseconds.\\n";
 
-    // 计时遍历整个vector的时间
+    // Time traversing the entire vector
     auto start_traverse = std::chrono::high_resolution_clock::now();
     for (auto it = bool_vector.begin(); it != bool_vector.end(); ++it) {
         if (*it) {
@@ -40,34 +42,36 @@ void benchOrig(){
     }
     auto end_traverse = std::chrono::high_resolution_clock::now();
     auto duration_traverse = std::chrono::duration_cast<std::chrono::milliseconds>(end_traverse - start_traverse);
-    std::cout << "Traversing all elements took " << duration_traverse.count() << " milliseconds.\n";
+    std::cout << "Traversing all elements took " << duration_traverse.count() << " milliseconds.\\n";
 
-    std::cout << "True count: " << true_count << "\n";
+    std::cout << "True count: " << true_count << "\\\\n";
 
 }
-void benchMy(){
-    constexpr size_t SIZE = 10000000;  // 1000万元素大小
 
-    // 定义一个vector<bool>
+// Benchmarks the performance of bowen::bitvector for setting, assigning, accessing, and traversing elements.
+void benchmarkBowenBitvector(){
+    constexpr size_t SIZE = 10000000;  // 10 million elements
+
+    // Define a bitvector
     bowen::bitvector bool_vector(SIZE);
 
-    // 计时设置每个元素为true的时间
+    // Time setting each element
     auto start_set = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < SIZE; ++i) {
         bool_vector[i] = i&0x1;
     }
     auto end_set = std::chrono::high_resolution_clock::now();
     auto duration_set = std::chrono::duration_cast<std::chrono::milliseconds>(end_set - start_set);
-    std::cout << "Setting all elements took " << duration_set.count() << " milliseconds.\n";
+    std::cout << "Setting all elements took " << duration_set.count() << " milliseconds.\\n";
 
-        // 计时设置每个元素为true的时间
+        // Time setting each element using assign
     auto start_assign = std::chrono::high_resolution_clock::now();
     bowen::bitvector vector2;
     vector2.assign(SIZE, 1);
     auto end_assign = std::chrono::high_resolution_clock::now();
     auto duration_assign = std::chrono::duration_cast<std::chrono::milliseconds>(end_assign - start_assign);
-    std::cout << "assign all elements took " << duration_assign.count() << " milliseconds.\n";
-    // 计时访问每个元素的时间
+    std::cout << "assign all elements took " << duration_assign.count() << " milliseconds.\\n";
+    // Time accessing each element
     size_t true_count = 0;
     auto start_access = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < SIZE; ++i) {
@@ -77,9 +81,9 @@ void benchMy(){
     }
     auto end_access = std::chrono::high_resolution_clock::now();
     auto duration_access = std::chrono::duration_cast<std::chrono::milliseconds>(end_access - start_access);
-    std::cout << "Accessing all elements took " << duration_access.count() << " milliseconds.\n";
+    std::cout << "Accessing all elements took " << duration_access.count() << " milliseconds.\\n";
 
-    // 计时遍历整个vector的时间
+    // Time traversing the entire vector
     auto start_traverse = std::chrono::high_resolution_clock::now();
     for (auto it = bool_vector.begin(); it != bool_vector.end(); ++it) {
         if (*it) {
@@ -88,17 +92,19 @@ void benchMy(){
     }
     auto end_traverse = std::chrono::high_resolution_clock::now();
     auto duration_traverse = std::chrono::duration_cast<std::chrono::milliseconds>(end_traverse - start_traverse);
-    std::cout << "Traversing all elements took " << duration_traverse.count() << " milliseconds.\n";
+    std::cout << "Traversing all elements took " << duration_traverse.count() << " milliseconds.\\n";
 
-    std::cout << "True count: " << true_count << "\n";
+    std::cout << "True count: " << true_count << "\\n";
 }
-void tests(){
-    constexpr size_t SIZE = 10000000;  // 1000万元素大小
 
-    // 定义一个vector<bool>
+// Compares the behavior of bowen::bitvector against std::vector<bool> for basic operations.
+void testBitvectorAgainstStdVectorBool(){
+    constexpr size_t SIZE = 10000000;  // 10 million elements
+
+    // Define a bitvector and a vector<bool>
     bowen::bitvector bool_vector1(SIZE);
     std::vector<bool> bool_vector2(SIZE);
-    // 计时设置每个元素为true的时间
+    // Time setting each element
     auto start_set = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < SIZE; ++i) {
         bool_vector1[i] = i&0x1;
@@ -106,10 +112,10 @@ void tests(){
     }
     auto end_set = std::chrono::high_resolution_clock::now();
     auto duration_set = std::chrono::duration_cast<std::chrono::milliseconds>(end_set - start_set);
-    std::cout << "Setting all elements took " << duration_set.count() << " milliseconds.\n";
+    std::cout << "Setting all elements took " << duration_set.count() << " milliseconds.\\n";
     assert(bool_vector1.size()==bool_vector2.size());
     assert(bool_vector1.empty() == bool_vector2.empty());
-    // 计时访问每个元素的时间
+    // Time accessing each element and comparing
     size_t false_count = 0;
     auto start_access = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < SIZE; ++i) {
@@ -119,17 +125,21 @@ void tests(){
     }
     auto end_access = std::chrono::high_resolution_clock::now();
     auto duration_access = std::chrono::duration_cast<std::chrono::milliseconds>(end_access - start_access);
-    std::cout << "Accessing all elements took " << duration_access.count() << " milliseconds.\n";
+    std::cout << "Accessing all elements took " << duration_access.count() << " milliseconds.\\n";
     std::cout << "found "<< false_count<< " error in bitvector" <<std::endl;
 
 
 }
-void testincrementUntilZero(){
-    constexpr size_t SIZE = 128*10;  // 1000万元素大小
 
-    // 定义一个vector<bool>
+// Tests the incrementUntilZero method of bowen::bitvector.
+// This method is expected to increment a counter starting from a given position
+// as long as it encounters '1's in the bitvector, stopping at the first '0' or the end of the vector.
+void testBitvectorIncrementUntilZero(){
+    constexpr size_t SIZE = 128*10;  // Size of the bitvector
+
+    // Define a bitvector
     bowen::bitvector bool_vector1(SIZE);
-    // 计时设置每个元素为true的时间
+    // Time setting some elements to true
     int start = rand()%(SIZE-2);
     int correct_count=start;
     int end = start+ rand()%(SIZE-start);
@@ -149,31 +159,15 @@ void testincrementUntilZero(){
 
     assert(count == correct_count);
 }
-void speedtest() {
-    uint64_t n = 4000000000;//357171682
-    //n = 100;
-    // 计时设置每个元素为true的时间
-    auto start_set = std::chrono::high_resolution_clock::now();
 
-
-    uint64_t primes;
-    for (int i = 0; i < 1; i++) {
-        sieve3(&primes, n, 1, 0);
-    }
-
-    auto end_set = std::chrono::high_resolution_clock::now();
-    auto duration_set = std::chrono::duration_cast<std::chrono::milliseconds>(end_set - start_set);
-    printf("prime count of %zd is : %zd\n", n, primes);
-    std::cout << "Setting all elements took " << duration_set.count() / 3 << " milliseconds.\n";
+void runAllTests() {
+    testBitvectorAgainstStdVectorBool();
+    testBitvectorIncrementUntilZero();
 }
-void testPrePrime() {
-    std::vector<uint64_t> primes;
-    calPrePrimes(primes, 9999, 10000);
-    for (auto p: primes) {
-        printf("%zd ", p);
-    }
-}
+
 int main() {
-    speedtest();
+    benchmarkStdVectorBool();
+    benchmarkBowenBitvector();
+    runAllTests();
     return 0;
 }
